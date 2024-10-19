64b95ab918022109ae2064c3a316adf23e35c18d
Kushini Lowe <115502398+k5lowe@users.noreply.github.com>
2024-10-19 14:45:43 -0400

    Add files via upload

---
+++ create_table_query.sql
@@ -0,0 +1,8 @@
+CREATE TABLE testing (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(64),
+    price VARCHAR(15),
+    `description` VARCHAR(250),
+    `address` VARCHAR(100),
+    `listing id` INT
+)
